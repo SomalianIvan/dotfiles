70f9a777db047e3d224d65cd71568c8192b94dce
Timur Borkhodoev <tim.borkhodoev@shopify.com>
2022-01-18 12:24:33 -0500

    wip: try one

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/zsh
+
+sudo add-apt-repository ppa:neovim-ppa/unstable -y
+sudo apt-get update
+sudo apt-get install neovim -y
+
+if ! command -v rg &> /dev/null; then
+  sudo apt-get install -y silversearcher-ag
+fi
+
+ln -sf ~/dotfiles/config.vim ~/.config/nvim/init.vim
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+cd
+git clone https://github.com/gpakosz/.tmux.git
+ln -s -f .tmux/.tmux.conf
+cp .tmux/.tmux.conf.local .
